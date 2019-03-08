b3a681afb513c4cafab69c83a6c17dcc22f346cf
Christian <christian@cke.no>
2019-03-08 11:23:03 +0100

    oppdatert for å fungere på raspberry pi med python 3.5

--- temp_logging.py
+++ temp_logging.py
@@ -1,23 +1,30 @@
-# Developed to run on python 3.7
+#This works on Rasperry pi running python3.5
 import serial
 import time
 from sys import argv
 
-testName = argv[1]
 
-logfile = 'Logs\\' + testName + '.txt'
+testName = input('Name the log file:\n>')
+
+logfile = "Logs/" + testName + ".csv"
 
 
-print(f"""Starting temperature logging.
-Logfile will be saved in {logfile}.
-To stop the temperature logging press Ctrl+c.\n""")
-with serial.Serial('COM7', 9600, timeout = 1) as ser:
+print("Starting temperature logging.\nLogfile will be saved in {}.\
+To stop the temperature logging press Ctrl+c.\n".format(logfile))
+
+# linux: change COM7 with: '/dev/ttyUSB0'
+with serial.Serial('/dev/ttyUSB0', 9600, timeout = 1) as ser:
     with open(logfile, 'w') as log:
+        header = "Time;Temp 1; Temp 2\n"
+        log.write(header)
         while 1:
             tempTime = time.localtime()
+            #h = str(tempTime[3]).zfill(2)
+            #m = str(tempTime[4]).zfill(2)
+            timestamp = time.strftime('%d-%m-%y %H:%m', tempTime)
             temp = ser.readline()
             temp = temp.decode('utf-8')
-            timestamp = f"{tempTime[3]}:{tempTime[4]} - {temp}"
-            print(timestamp)
-            log.write(timestamp)
-            time.sleep(0.5)
+            logline = "{};{}\n".format(timestamp,temp)
+            print("{} - {}".format(timestamp, temp))
+            log.write(logline)
+            time.sleep(600)
